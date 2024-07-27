fd0c2c4d98895744e148c8a10b9d78826a9b9f25
Tianyu Wu <ddwty@163.com>
2024-07-27 23:18:24 +0800

    Initial Commit

---
+++ RoboticDataCollection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RoboticDataCollection
+//
+//  Created by 吴天禹 on 2024/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RoboticDataCollection/RoboticDataCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoboticDataCollectionApp.swift
+//  RoboticDataCollection
+//
+//  Created by 吴天禹 on 2024/7/27.
+//
+
+import SwiftUI
+
+@main
+struct RoboticDataCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
